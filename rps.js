9fb964fb52490ab47790fb71dfb42fda57025fb9
berkaykarlik <brkykrlk@gmail.com>
2021-11-20 23:13:14 +0300

    add: UI

--- rps.js
+++ rps.js
@@ -1,66 +1,108 @@
 
 const AVAILABLE_MOVES = ['rock','scissors','paper'];
-const NUMBER_OF_ROUNDS = 5 ;
 const PLAYER_WINS = 1 ;
 const DRAW = 0 ;
 const COMPUTER_WINS = -1 ;
+
+const game_container = document.querySelector('#game-container');
+const buttons = document.querySelectorAll('.control-btn');
+const user_score = document.querySelector('#user-side .score');
+const user_choice = document.querySelector('#user-side .choice');
+const com_score = document.querySelector('#com-side .score');
+const com_choice = document.querySelector('#com-side .choice');
+
+
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     return AVAILABLE_MOVES[Math.floor(Math.random() * AVAILABLE_MOVES.length)];
 }
 
-function playRound(playerSelection ,computerSelection ) {
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
     if (playerSelection == computerSelection){
-        return DRAW;
+        updateGameState(playerSelection,computerSelection,DRAW);
+        return;
     }
     else if ((AVAILABLE_MOVES.indexOf(playerSelection) + 1) % AVAILABLE_MOVES.length  == AVAILABLE_MOVES.indexOf(computerSelection)){
-        return PLAYER_WINS;
+        playerScore += 1;
+        updateGameState(playerSelection,computerSelection,PLAYER_WINS);
+        return;
     }
-    return COMPUTER_WINS;
+    computerScore += 1;
+    updateGameState(playerSelection,computerSelection,COMPUTER_WINS);
+    return;
 }
 
-function game() {
-    // for debug
-    function outcome2string (outcome) {
-        switch (outcome){
-            case PLAYER_WINS:
-                return 'player wins'
-            case DRAW:
-                return 'draw'
-            case COMPUTER_WINS:
-                return 'computer wins'
-            default:
-                console.log('invalid input')
-                return 'none'
-        }
+function updateGameState(playerSelection,computerSelection,outcome) {
+    let user_color;
+    let com_color;
+    switch (outcome){
+        case PLAYER_WINS:
+            user_color= 'green';
+            com_color = 'red';
+            break;
+        case COMPUTER_WINS:
+            user_color= 'red';
+            com_color = 'green';
+            break;
+        case DRAW:
+            user_color= 'gray';
+            com_color = 'gray';
     }
-    let computerSelection;
-    let playerSelection;
-    let outcome;
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
-        //player input
-        do {
-            playerSelection = prompt('write "rock","paper" or "scissors"');
-            playerSelection = playerSelection.toLowerCase();
-            isInvalid = !AVAILABLE_MOVES.includes(playerSelection);
-            if (isInvalid){
-                alert('invalid input');
-            }
-        }
-        while (isInvalid);
-        //computer input
-        computerSelection = computerPlay();
-        //play a round
-        outcome = playRound(playerSelection ,computerSelection );
-        //declare round winner
-        console.log('computer choose ' + computerSelection + ' player choose ' + playerSelection + ' outcome: ' + outcome2string(outcome));
-        //update total score
-        outcome > 0 ? playerScore +=1 : computerScore +=1 ;
-      }
-    alert('game over \nplayer score: ' + playerScore + '\ncomputer score: ' + computerScore);
+
+    //update scores
+    user_score.textContent = 'User: ' + playerScore;
+    com_score.textContent = 'Com: ' + computerScore;
+    //update choices
+    user_choice.textContent = playerSelection;
+    com_choice.textContent = computerSelection;
+
+    //update colors
+    user_score.style.color = user_color;
+    user_choice.style.color = user_color;
+    com_score.style.color = com_color;
+    com_choice.style.color = com_color;
+
+    if (playerScore == 5){
+        gameOver('player');
+    }
+    else if (computerScore == 5){
+        gameOver('computer');
+    }
 }
 
+function gameOver(winner) {
+    let message = winner + ' won';
 
-game();
+    while (game_container.firstChild) {
+        game_container.removeChild(game_container.lastChild);
+    }
+
+    let end_game_div =  document.createElement('div');
+    end_game_div.style.display = 'flex';
+    end_game_div.style.flexDirection = 'column';
+    end_game_div.style.justifyContent = 'center';
+    end_game_div.style.alignItems = 'center';
+
+    let winner_h =  document.createElement('h2');
+    winner_h.textContent = message;
+
+    let play_again_h =  document.createElement('button');
+    play_again_h.textContent = 'Play again ?';
+
+    play_again_h.addEventListener('click', () => {
+        location.reload(); //reload page so that game restarts, I know not the most efficient method
+    });
+
+    end_game_div.appendChild(winner_h);
+    end_game_div.appendChild(play_again_h);
+    game_container.appendChild(end_game_div);
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        playRound(button.id);
+    });
+});
